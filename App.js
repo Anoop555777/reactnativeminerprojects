b5cbe3dd1c7906878acfd6589a16aee3ce402c59
Anoop555777 <ajbisht99@gmail.com>
2023-04-06 23:19:57 +0530

    initial

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enterList, setEnterList] = useState("");
+  const [listContainer, setListContainer] = useState([]);
+
+  const enterInputHandler = (enterName) => {
+    setEnterList(enterName);
+  };
+
+  const addListHandler = () => {
+    setListContainer((prevState) => [
+      ...prevState,
+      { value: enterList, id: Math.random() },
+    ]);
+    setEnterList("");
+  };
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text>hi</Text>
+      <View style={styles.textField}>
+        <TextInput
+          placeholder="Enter value"
+          style={styles.InputText}
+          onChangeText={enterInputHandler}
+          value={enterList}
+        />
+        <Button title="AddToList" color="purple" onPress={addListHandler} />
       </View>
-      <Button
-        title="click me
-      "
-      />
-      <Text>my name is anoop !</Text>
-
-      <StatusBar style="auto" />
+      <View style={styles.Lists}>
+        <FlatList
+          data={listContainer}
+          renderItem={(itemData) => (
+            <View style={styles.listItem}>
+              <Text style={styles.listText}>{itemData.item.value}</Text>
+            </View>
+          )}
+          alwaysBounceVertical={false}
+          keyExtractor={(item, index) => item.id}
+        />
+      </View>
     </View>
   );
 }
@@ -24,5 +58,38 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 24,
+  },
+  textField: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 2,
+  },
+  InputText: {
+    borderWidth: 1,
+    width: "70%",
+    marginRight: 12,
+    padding: 3,
+    paddingLeft: 5,
+  },
+  Lists: {
+    flex: 5,
+    width: "100%",
+    marginTop: 16,
+  },
+  listItem: {
+    backgroundColor: "purple",
+    width: "100%",
+    alignItems: "center",
+
+    borderRadius: 11,
+    marginBottom: 12,
+    paddingVertical: 8,
+  },
+  listText: {
+    color: "#fff",
   },
 });
